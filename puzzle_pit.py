f88a85703b7a2327075001e7a62957f0b767e99a
frnkthtnk100 <fpettigrosso@holyfamily.edu>
2018-09-28 15:53:02 -0400

    do the move thing

--- puzzle_pit.py
+++ puzzle_pit.py
@@ -26,4 +26,39 @@ class puzzle_pit:
     def is_completed(self):
         return "-1" not in self.to_string()
     
-        
+    def move(block, direction):
+        '''
+           h - left
+           j - down
+           k - up
+           l - right
+        '''
+        temp_matrix = self.clone()
+        #validate block
+        move_possible = False
+        if block in self.to_string():
+            cordinates = self.getblock(block)
+            if direction == 'h':
+                for cord in cordinates:
+                    if validate( cord[0], cord[1]):
+                        
+
+        else:
+            print("invalid block") 
+    
+    def getblock(self, block):
+        cords = []
+        for i in range(0, self.height):
+            for j in range(0, self.width):
+                if self.matrix[i][j] == block:
+                    cords.append([i,j])
+        return cords
+
+    def validate(self, y, x):
+        return 0 <= y < self.height and 0 <= x < self.width 
+
+    def move(self, matrix , original_y, original_x, new_y, new_x, block):
+        matrix[new_y][new_x] += matrix[original_y][original_y]
+        matrix[original_y][original_y] = 0
+        return matrix
+
